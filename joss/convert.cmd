0347492e4500430f5cbaefa628db6851fb566827
Noah Pflugradt <n.pflugradt@fz-juelich.de>
2021-04-07 09:31:46 +0200

    paper update

--- joss/convert.cmd
+++ joss/convert.cmd
@@ -1,2 +1,3 @@
-pandoc paper.md -o p.pdf --verbose --bibliography=references.bib --fail-if-warnings --log=p.log > t1.txt
+rem pandoc paper.md -o p.pdf --verbose --bibliography=references.bib --fail-if-warnings --log=p.log > t1.txt
+pandoc paper.md -o p.docx --verbose --bibliography=references.bib --fail-if-warnings --log=p.log > t1.txt
 pause
