78c52622b73622c5c38771b440444417687d24f4
d.neuroth <d.neuroth@fz-juelich.de>
2022-09-27 16:11:06 +0200

    added /D flag to cd commands in copynewToPython.cmd to allow changing drives

--- copynewToPython.cmd
+++ copynewToPython.cmd
@@ -4,26 +4,26 @@ set "vsdirectory=D:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild
 
 REM copy V:\Dropbox\LPG\WpfApplication1\profilegenerator-latest.db3 c:\Main\Git-Repositories\LoadProfileGenerator\WpfApplication1\profilegenerator-latest.db3
 
-cd %srcdirectory%
+cd /D %srcdirectory%
 %srcdirectory%\VersionIncreaser\bin\Debug\versionincreaser.exe
 
-cd %srcdirectory%\SimulationEngine
+cd /D %srcdirectory%\SimulationEngine
 rmdir /S /Q %srcdirectory%\SimulationEngine\bin
 "%vsdirectory%\msbuild.exe" SimulationEngine.csproj -t:rebuild -v:m
 
-cd %srcdirectory%\WpfApplication1
+cd /D %srcdirectory%\WpfApplication1
 rmdir /S/Q %srcdirectory%\WpfApplication1\bin
 "%vsdirectory%\msbuild.exe" LoadProfileGenerator.csproj -t:rebuild  -v:m
 
-cd %srcdirectory%\SimEngine2
+cd /D %srcdirectory%\SimEngine2
 rmdir /S /Q %srcdirectory%\SimEngine2\bin
 dotnet publish simengine2.csproj --configuration Release --self-contained true --runtime win10-x64 --verbosity quiet -f net6.0-windows
 dotnet publish simengine2.csproj --configuration Release --self-contained true --runtime linux-x64 --verbosity quiet -f net6.0
 
-cd %srcdirectory%\ReleaseMaker
+cd /D %srcdirectory%\ReleaseMaker
 "%vsdirectory%\msbuild.exe" ReleaseMaker.csproj -t:rebuild  -v:m
 
-cd %srcdirectory%\ReleaseMaker\bin\Debug\net6.0-windows
+cd /D %srcdirectory%\ReleaseMaker\bin\Debug\net6.0-windows
 releasemaker
 pause
 
@@ -32,7 +32,7 @@ REM create pylpg
 set "releasedirectory=C:\LPGReleaseMakerResults\LPGReleases\releases10.9.0"
 set "pylpgdirectory=C:\LPGPythonBindings\pylpg\"
 
-cd %releasedirectory%\net48
+cd /D %releasedirectory%\net48
 simulationengine cpy
 xcopy lpgdata.py %pylpgdirectory%
 xcopy lpgpythonbindings.py %pylpgdirectory%
