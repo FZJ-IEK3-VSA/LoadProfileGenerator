0073776a46c96c544725ccbbff13be945debbb18
David Neuroth <d.neuroth@fz-juelich.de>
2024-06-19 13:10:03 +0200

    created new empty project MassSimulation

---
+++ MassSimulation/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MassSimulation
+{
+    internal static class Program
+    {
+
+        public static void Main([NotNull] string[] args)
+        {
+            Console.WriteLine("LPG Mass Simulation");
+        }
+    }
+}
