6451b51069b80dcf461397166878f9ded639aaa3
David Neuroth <d.neuroth@fz-juelich.de>
2024-09-30 21:27:05 +0200

    added an empty PointOfInterestSimulator class

---
+++ MassSimulation/Simulators/PointOfInterestSimulator.cs
@@ -0,0 +1,36 @@
+﻿using CalculationEngine.CitySimulation;
+using Common;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MassSimulation.Simulators
+{
+    /// <summary>
+    /// Simulates agent stays in any point of interest, for example a small enterprise.
+    /// </summary>
+    internal class PointOfInterestSimulator : ISimulator
+    {
+        private Collection<AgentStayState> presentAgents = [];
+
+        public PointOfInterestId PoiId => throw new NotImplementedException();
+
+        public void FinishSimulation()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<RemoteActivityInfo> SimulateOneStep(TimeStep timeStep, DateTime dateTime)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<RemoteActivityFinished> SimulateOneStep(TimeStep timeStep, DateTime dateTime, IEnumerable<RemoteActivityStart> newActivities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
