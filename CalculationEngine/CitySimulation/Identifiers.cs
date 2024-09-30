3110f300ea0139ba081387c797fe700df4cbcb9e
David Neuroth <d.neuroth@fz-juelich.de>
2024-09-30 18:31:01 +0200

    added object identifier classes and activity message classes for the city simulation

---
+++ CalculationEngine/CitySimulation/Identifiers.cs
@@ -0,0 +1,30 @@
+﻿using Automation.ResultFiles;
+
+namespace CalculationEngine.CitySimulation
+{
+    /// <summary>
+    /// Unique identifier for a CalcPerson in the city simulation
+    /// </summary>
+    /// <param name="personName">the name of the person</param>
+    /// <param name="householdKey">the household key</param>
+    /// <param name="targetId">the simulation target, i.e. the building</param>
+    /// <param name="workerId">the rank of the worke that simulates the target</param>
+    public class PersonIdentifier(string personName, HouseholdKey householdKey, string targetId, int workerId)
+    {
+        public readonly string PersonName = personName;
+        public readonly HouseholdKey HouseholdKey = householdKey;
+        public readonly string TargetId = targetId;
+        public readonly int WorkerId = workerId;
+    }
+
+    /// <summary>
+    /// Unique identifier for a point of interest and its simulator.
+    /// </summary>
+    /// <param name="id">ID of the point of interest</param>
+    /// <param name="workerId">rank of the worker that simulates the POI</param>
+    public class PointOfInterestId(string id, int workerId)
+    {
+        public readonly string Id = id;
+        public readonly int WorkerId = workerId;
+    }
+}
---
+++ CalculationEngine/CitySimulation/Messages.cs
@@ -0,0 +1,40 @@
+﻿using Automation.ResultFiles;
+using CalculationEngine.HouseholdElements;
+
+namespace CalculationEngine.CitySimulation
+{
+    /// <summary>
+    /// Object encapsulating relevant information for a new remote activity
+    /// </summary>
+    /// <param name="person">the person carrying out the activity</param>
+    /// <param name="affordanceActivation">the corresponding RemoteAffordanceActivation object</param>
+    public class RemoteActivityInfo(PersonIdentifier person, RemoteAffordanceActivation affordanceActivation)
+    {
+        public readonly PersonIdentifier Person = person;
+        public readonly RemoteAffordanceActivation AffordanceActivation = affordanceActivation;
+    }
+
+    /// <summary>
+    /// A message that the current remote activity of a person has been finished.
+    /// </summary>
+    /// <param name="person">the person whose activity is finished</param>
+    public class RemoteActivityFinished(PersonIdentifier person)
+    {
+        public readonly PersonIdentifier Person = person;
+    }
+
+    /// <summary>
+    /// A message that a person is starting a new remote activity.
+    /// </summary>
+    /// <param name="person">the person starting the activity</param>
+    /// <param name="isTravel">whether the activity is traveling or an activity at a point of interest</param>
+    /// <param name="affordance">the name of the affordance being activated</param>
+    /// <param name="poi">the ID of the point of interest where the activity will be carried out</param>
+    public class RemoteActivityStart(PersonIdentifier person, bool isTravel, string affordance, PointOfInterestId? poi = null)
+    {
+        public readonly PersonIdentifier Person = person;
+        public readonly bool IsTravel = isTravel;
+        public readonly string Affordance = affordance;
+        public readonly PointOfInterestId? Poi = poi;
+    }
+}
