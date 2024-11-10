fa4d24dbadc3f53fec4f7d26fac35b7a4fe0f381
David Neuroth <d.neuroth@fz-juelich.de>
2024-11-10 21:38:14 +0100

    moved dictionary extension class to the Common project

--- Common/DictionaryExtensions.cs
+++ Common/DictionaryExtensions.cs
@@ -16,7 +16,7 @@ namespace MassSimulation
         /// <typeparam name="TValue">dictionary value type</typeparam>
         /// <param name="dict">the dictionary to get a value from</param>
         /// <param name="key">the key of the value to get</param>
-        /// <returns></returns>
+        /// <returns>the (possibly newly created) value with the specified key</returns>
         public static TValue GetOrAddDefault<TKey, TValue>(this Dictionary<TKey, TValue> dict, TKey key) where TKey : notnull where TValue : new()
         {
             if (!dict.TryGetValue(key, out var value))
