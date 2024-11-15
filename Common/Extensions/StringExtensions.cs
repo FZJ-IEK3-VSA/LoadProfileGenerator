e4780db20cdb0288f2c6fab7b1c03972f599f46e
David Neuroth <d.neuroth@fz-juelich.de>
2024-11-15 17:02:37 +0100

    added a new string extension method RemoveSuffix

---
+++ Common/Extensions/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace Common.Extensions
+{
+    /// <summary>
+    /// Defines string extension methods
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// If this string ends with the specified suffix, removes the suffix
+        /// and returns the resulting substring.
+        /// </summary>
+        /// <param name="s">the original string</param>
+        /// <param name="suffix">the suffix to remove</param>
+        /// <returns>the substring without the suffix</returns>
+        public static string RemoveSuffix(this string s, string suffix)
+        {
+            if (s.EndsWith(suffix))
+            {
+                return s.Substring(0, s.Length - suffix.Length);
+            }
+            return s;
+        }
+    }
+}
