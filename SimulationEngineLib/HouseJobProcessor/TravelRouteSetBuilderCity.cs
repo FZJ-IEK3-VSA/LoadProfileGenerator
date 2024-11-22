adc29b46048f282b12e5acacffb36b978a14caf6
David Neuroth <d.neuroth@fz-juelich.de>
2024-11-22 11:32:07 +0100

    created mirrored routes, fix route creation, add default route weight

--- SimulationEngineLib/HouseJobProcessor/TravelRouteSetBuilderCity.cs
+++ SimulationEngineLib/HouseJobProcessor/TravelRouteSetBuilderCity.cs
@@ -55,17 +55,33 @@ namespace SimulationEngineLib.HouseJobProcessor
             var person = sim.Persons.FindFirstByNameNotNull(personName);
             foreach (var routeData in preferences.Routes)
             {
+                // create the new travel route
                 var route = sim.TravelRoutes.CreateNewItem(sim.ConnectionString);
-                route.Name = $"Route for {personName} from {routeData.Start} to {routeData.Destination}";
-                route.Description = "Generated";
+                route.Description = "Generated from POI preferences";
                 route.SiteA = GetSiteFromPoi(routeData.Start);
-                route.SiteA = GetSiteFromPoi(routeData.Destination);
-                
+                route.SiteB = GetSiteFromPoi(routeData.Destination);
+                route.RouteKey = "Generated";
+
+                // create a single step with the specified transportation device category
                 var deviceCategory = sim.TransportationDeviceCategories.FindWithException(routeData.TransportationDeviceCategory);
                 var name = deviceCategory.Name;
                 route.AddStep(name, deviceCategory, routeData.Distance, 1, name, true);
-                
+
+                SetRouteName(route, personName, deviceCategory.Name);
+                route.SaveToDB();
                 travelRouteSet.AddRoute(route, personID: person.IntID, weight: routeData.Weight);
+
+                // if required, also create an identical route in the opposite direction
+                if (preferences.MirrorRoutes)
+                {
+                    var mirroredRoute = route.MakeACopy(sim);
+                    mirroredRoute.SiteA = route.SiteB;
+                    mirroredRoute.SiteB = route.SiteA;
+                    SetRouteName(mirroredRoute, personName, deviceCategory.Name);
+
+                    mirroredRoute.SaveToDB();
+                    travelRouteSet.AddRoute(mirroredRoute, personID: person.IntID, weight: routeData.Weight);
+                }
             }
         }
 
@@ -79,6 +95,18 @@ namespace SimulationEngineLib.HouseJobProcessor
             if (!LocationReplacements.TryGetValue(poiId, out var routeData))
                 throw new LPGException("The PointOfInterestPreferences specify a route with start/destination {poiId}, which is no known site or POI.");
             return routeData.NewSite;
+        }
+
+        /// <summary>
+        /// Sets the name for a travel route depending on start and destination, and the name
+        /// of the person the route is made for.
+        /// </summary>
+        /// <param name="route">the route to set the name for</param>
+        /// <param name="personName">the name of the person who will use the route</param>
+        private static void SetRouteName(TravelRoute route, string personName, string deviceCategoryName)
+        {
+            var distanceInKm = route.CalculateTotalDistance() / 1000;
+            route.Name = $"Route for {personName} from {route.SiteA.Name} to {route.SiteB.Name} via {deviceCategoryName} {distanceInKm}km";
         }
     }
 }
