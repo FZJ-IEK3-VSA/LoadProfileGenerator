952337a44aee68dee57f369a323467d166c65168
David Neuroth <d.neuroth@fz-juelich.de>
2024-12-16 19:05:45 +0100

    moved CitySimWrapperException into its own file

---
+++ MassSimulation/CitySimWrapperException.cs
@@ -0,0 +1,21 @@
+﻿namespace MassSimulation
+{
+    /// <summary>
+    /// Wrapper exception that can be used to add relevant information about the part of the simulation that caused
+    /// an exception.
+    /// </summary>
+    internal class CitySimWrapperException(Exception ex, int worker, MassSimulationTarget? target = null, int timestep = -1)
+        : Exception(BuildExceptionMessage(ex, worker, target, timestep), ex)
+    {
+        public int Worker { get; } = worker;
+        public MassSimulationTarget? Target { get; } = target;
+        public int Timestep { get; } = timestep;
+
+        private static string BuildExceptionMessage(Exception ex, int worker, MassSimulationTarget? target, int timestep)
+        {
+            var targetString = target is not null ? $" from target '{target.Id}'" : "";
+            var timestepString = timestep != -1 ? $" in timestep {timestep}" : "";
+            return $"Exception on worker {worker}{targetString}{timestepString}: {ex.Message}";
+        }
+    }
+}
--- MassSimulation/LPGMassSimulator.cs
+++ MassSimulation/LPGMassSimulator.cs
@@ -15,22 +15,6 @@ using System.Runtime.InteropServices;
 
 namespace MassSimulation
 {
-    internal class CitySimWrapperException(Exception ex, int worker, MassSimulationTarget? target = null, int timestep = -1)
-        : Exception(BuildExceptionMessage(ex, worker, target, timestep), ex)
-    {
-        public MassSimulationTarget? Target { get; } = target;
-        public int Worker { get; } = worker;
-        public int Timestep { get; } = timestep;
-
-        private static string BuildExceptionMessage(Exception ex, int worker, MassSimulationTarget? target, int timestep)
-        {
-            var targetString = target is not null ? $" from target '{target.Id}'" : "";
-            var timestepString = timestep != -1 ? $" in timestep {timestep}" : "";
-            return $"Exception on worker {worker}{targetString}{timestepString}: {ex.Message}";
-        }
-    }
-
-
     /// <summary>
     /// A class that simulates multiple LPG households simultaneously.
     /// </summary>
