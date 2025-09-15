e21d480cb38b2edafd7751f0522935037c123327
David Neuroth <d.neuroth@fz-juelich.de>
2025-09-15 13:13:06 +0200

    added optional index property to RouteData

--- Automation/RouteData.cs
+++ Automation/RouteData.cs
@@ -20,9 +20,11 @@ namespace Automation
     /// <param name="prob_no_car_hh">general mode choice probabilities if the household has no car</param>
     /// <param name="prob_no_car_hh_pt_abo">mode choice probabilities if the household has no car, but a public transport abo</param>
     /// <param name="prob_no_car_hh_no_pt_abo">mode choice probabilities if the household has no car and no public transport abo</param>
+    /// <param name="index">an optional index to identify this RouteData object</param>
     public record RouteData(string origin_id, string destination_id, Dictionary<string, double?> mode_times,
         Dictionary<string, double?> mode_distances, Dictionary<string, double> prob_all,
         Dictionary<string, double> prob_with_car_hh, Dictionary<string, double> prob_with_car_hh_pt_abo,
         Dictionary<string, double> prob_with_car_hh_no_pt_abo, Dictionary<string, double> prob_no_car_hh,
-        Dictionary<string, double> prob_no_car_hh_pt_abo, Dictionary<string, double> prob_no_car_hh_no_pt_abo);
+        Dictionary<string, double> prob_no_car_hh_pt_abo, Dictionary<string, double> prob_no_car_hh_no_pt_abo,
+        int index = -1);
 }
