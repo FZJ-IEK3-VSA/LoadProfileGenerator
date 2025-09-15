da1b6d9db0a7251df1cbc2ffe5f4c796122d2def
David Neuroth <d.neuroth@fz-juelich.de>
2025-09-15 13:39:24 +0200

    added missing optional properties to RouteData, and made some of the currently unused prob dicts nullable

--- Automation/RouteData.cs
+++ Automation/RouteData.cs
@@ -15,16 +15,21 @@ namespace Automation
     /// <param name="mode_distances">travel distances per mode in km</param>
     /// <param name="prob_all">general mode choice probabilities</param>
     /// <param name="prob_with_car_hh">general mode choice probabilities if the household has a car</param>
-    /// <param name="prob_with_car_hh_pt_abo">mode choice probabilities if the household has a car and public transport abo</param>
-    /// <param name="prob_with_car_hh_no_pt_abo">mode choice probabilities if the household has a car, but no public transport abo</param>
     /// <param name="prob_no_car_hh">general mode choice probabilities if the household has no car</param>
+    /// <param name="prob_with_car_hh_pt_abo">mode choice probabilities if the household has a car and public transport abo</param>
     /// <param name="prob_no_car_hh_pt_abo">mode choice probabilities if the household has no car, but a public transport abo</param>
+    /// <param name="prob_with_car_hh_no_pt_abo">mode choice probabilities if the household has a car, but no public transport abo</param>
     /// <param name="prob_no_car_hh_no_pt_abo">mode choice probabilities if the household has no car and no public transport abo</param>
     /// <param name="index">an optional index to identify this RouteData object</param>
+    /// <param name="logsum_util_all">optional data from transport model</param>
+    /// <param name="logsum_util_no_car_hh">optional data from transport model</param>
+    /// <param name="logsum_util_with_car_hh">optional data from transport model</param>
+    /// <param name="logsum_time">optional data from transport model</param>
     public record RouteData(string origin_id, string destination_id, Dictionary<string, double?> mode_times,
         Dictionary<string, double?> mode_distances, Dictionary<string, double> prob_all,
-        Dictionary<string, double> prob_with_car_hh, Dictionary<string, double> prob_with_car_hh_pt_abo,
-        Dictionary<string, double> prob_with_car_hh_no_pt_abo, Dictionary<string, double> prob_no_car_hh,
-        Dictionary<string, double> prob_no_car_hh_pt_abo, Dictionary<string, double> prob_no_car_hh_no_pt_abo,
-        int index = -1);
+        Dictionary<string, double> prob_with_car_hh, Dictionary<string, double> prob_no_car_hh,
+        Dictionary<string, double>? prob_with_car_hh_pt_abo, Dictionary<string, double>? prob_no_car_hh_pt_abo,
+        Dictionary<string, double>? prob_with_car_hh_no_pt_abo, Dictionary<string, double>? prob_no_car_hh_no_pt_abo,
+        int index = -1, double logsum_util_all = -1, double logsum_util_no_car_hh = -1, double logsum_util_with_car_hh = -1,
+        double logsum_time = -1);
 }
