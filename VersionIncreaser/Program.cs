e2b0dd24673cfb809854a91f21c047977426d2bc
David Neuroth <d.neuroth@fz-juelich.de>
2026-07-31 10:35:21 +0200

    cleanup and documentation in VersionIncreaser

--- VersionIncreaser/Program.cs
+++ VersionIncreaser/Program.cs
@@ -1,45 +1,59 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 using System.IO;
 
 namespace VersionIncreaser
 {
     public static class Program
     {
-        [SuppressMessage("ReSharper", "PossibleNullReferenceException")]
+        /// <summary>
+        /// Increments the build number in the AssemblyVersion tag in the Directory.Build.props file
+        /// and in the VersionInfo.cs file.
+        /// </summary>
+        /// <exception cref="Exception">if one of the files is missing or has an unexpected format</exception>
         public static void Main()
         {
             DirectoryInfo di = new DirectoryInfo(Environment.CurrentDirectory);
-            var fis = di.GetFiles("Directory.Build.props");
-            if (fis.Length != 1) {
-                throw new Exception("build.props not found.");
+            var buildPropsFiles = di.GetFiles("Directory.Build.props");
+            if (buildPropsFiles.Length != 1)
+            {
+                throw new Exception("File Directory.Build.props not found.");
             }
-            StreamReader sr = new StreamReader(fis[0].FullName);
+
+            // find and parse the current assembly version from the build props file
+            StreamReader sr = new StreamReader(buildPropsFiles[0].FullName);
             string s = sr.ReadLine();
-            while (!s.Contains("AssemblyVersion")) {
+            while (!s.Contains("AssemblyVersion"))
+            {
                 s = sr.ReadLine();
             }
             sr.Close();
             s = s.Replace("<AssemblyVersion>", "").Replace("</AssemblyVersion>", "").Trim();
-            var arr = s.Split('.');
-            var build = Convert.ToInt32(arr[3]);
+            var numbers = s.Split('.');
+            var build = Convert.ToInt32(numbers[3]);
+
+            // increment build number and create new assembly version string
             build = build + 1;
-            var newversion = arr[0] + "." + arr[1] + "." + arr[2] + "." + build.ToString();
-            var fileContent = File.ReadAllText(fis[0].FullName);
+            var newversion = $"{numbers[0]}.{numbers[1]}.{numbers[2]}.{build}";
+
+            // write the new version into the build props file
+            var fileContent = File.ReadAllText(buildPropsFiles[0].FullName);
             var newFileContent = fileContent.Replace(s, newversion);
             Console.WriteLine(newFileContent);
-            File.WriteAllText(fis[0].FullName,newFileContent);
+            File.WriteAllText(buildPropsFiles[0].FullName, newFileContent);
             Console.WriteLine("Increased version from " + s + " to " + newversion);
-            var fis2 = di.GetFiles("VersionInfo.cs");
-            if (fis2.Length != 1)
+
+            // find and open the version info file
+            var versionInfoFiles = di.GetFiles("VersionInfo.cs");
+            if (versionInfoFiles.Length != 1)
             {
-                throw new Exception("build.props not found.");
+                throw new Exception("File VersionInfo.cs not found.");
             }
-            var fis1 = di.GetFiles("VersionInfo.cs");
-            var fileContent1 = File.ReadAllText(fis1[0].FullName);
-            var newFileContent1 = fileContent1.Replace(s, newversion);
-            File.WriteAllText(fis1[0].FullName, newFileContent1);
-            Console.WriteLine(newFileContent1);
+
+            // replace the version string with the updated version
+            var versionInfoContent = File.ReadAllText(versionInfoFiles[0].FullName);
+            var newVersionInfoContent = versionInfoContent.Replace(s, newversion);
+            File.WriteAllText(versionInfoFiles[0].FullName, newVersionInfoContent);
+            Console.WriteLine(newVersionInfoContent);
         }
     }
 }
