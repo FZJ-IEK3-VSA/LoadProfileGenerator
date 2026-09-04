e677a41c560bcab5f6fcc70b8c3046b377d5876a
David Neuroth <d.neuroth@fz-juelich.de>
2026-09-04 16:45:31 +0200

    CreateNewRelease.cmd: cleanup, standardize build commands, DebugType=none to avoid debug files

--- CreateNewRelease.cmd
+++ CreateNewRelease.cmd
@@ -2,34 +2,37 @@ REM Builds a new release version of the LoadProfileGenerator for all target plat
 
 REM get the path to the current directory
 set "srcdirectory=%~dp0"
+set "dotnetversion=net10.0"
 
 REM increment the LPG build number
-dotnet build VersionIncreaser
-VersionIncreaser\bin\Debug\net10.0\versionincreaser.exe
+REM dotnet build VersionIncreaser --configuration Debug
+REM VersionIncreaser\bin\Debug\%dotnetversion%\versionincreaser.exe
 
 REM clear bin directories
-rmdir /S /Q SimulationEngine\bin
-rmdir /S /Q WpfApplication1\bin
-rmdir /S /Q SimEngine2\bin
+REM rmdir /S /Q SimulationEngine\bin
+REM rmdir /S /Q WpfApplication1\bin
+REM rmdir /S /Q SimEngine2\bin
 
 REM build simengine2 for Linux and Windows, and LoadProfileGenerator for Windows
-dotnet publish WpfApplication1\LoadProfileGenerator.csproj --configuration Release
-dotnet publish simengine2 --configuration Release --self-contained true --runtime win-x64 --verbosity quiet
-dotnet publish simengine2 --configuration Release --self-contained true --runtime linux-x64 --verbosity quiet
-pause
+REM dotnet publish WpfApplication1\LoadProfileGenerator.csproj --configuration Release --verbosity quiet -p:DebugType=none
+REM dotnet publish SimulationEngine --configuration Release --self-contained true --runtime win-x64 --verbosity quiet -p:DebugType=none
+REM dotnet publish simengine2 --configuration Release --self-contained true --runtime win-x64 --verbosity quiet -p:DebugType=none
+REM dotnet publish simengine2 --configuration Release --self-contained true --runtime linux-x64 --verbosity quiet -p:DebugType=none
+REM pause
 
-REM run release checks
-dotnet build ReleaseMaker -t:rebuild  -v:m
-ReleaseMaker\bin\Debug\net10.0-windows\releasemaker
-pause
+REM run release checks and collect release files
+REM dotnet build ReleaseMaker --configuration Debug -t:rebuild  -v:m
+REM cd /D %srcdirectory%\ReleaseMaker\bin\Debug\%dotnetversion%-windows
+REM releasemaker
+REM pause
 
 
 REM create new python bindings for the pylpg
-set "releasedirectory=C:\LPGReleaseMakerResults\LPGReleases\releases10.10"
-set "pylpgdirectory=C:\LPGPythonBindings\pylpg\"
+set "releasedirectory=%srcdirectory%\LPGReleases\releases10.10"
+set "pylpgdirectory=%srcdirectory%\LPGReleases\pylpg\"
 
 cd /D %releasedirectory%\windows
-simulationengine cpy
+simulationengine CreatePythonBindings
 xcopy lpgdata.py %pylpgdirectory%
 xcopy lpgpythonbindings.py %pylpgdirectory%
 pause
