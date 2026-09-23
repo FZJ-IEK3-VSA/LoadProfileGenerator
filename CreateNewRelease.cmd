b0b454fb0e2a8c13f383acd7b9124d6f404f6cf4
David Neuroth <d.neuroth@fz-juelich.de>
2026-09-23 15:49:23 +0200

    add error handling to CreateNewRelease.cmd

--- CreateNewRelease.cmd
+++ CreateNewRelease.cmd
@@ -1,8 +1,9 @@
 REM Builds a new release version of the LoadProfileGenerator for all target platforms, and creates new python bindings
 
-REM get the path to the current directory
+REM use the repository root as working directory
 set "srcdirectory=%~dp0"
 set "dotnetversion=net10.0"
+cd /D %srcdirectory%
 
 REM increment the LPG build number
 REM dotnet build VersionIncreaser --configuration Debug
@@ -15,16 +16,16 @@ rmdir /S /Q LoadProfileGenerator\bin
 rmdir /S /Q SimEngine2\bin
 
 REM build simengine2 for Linux and Windows, and LoadProfileGenerator for Windows
-dotnet publish LoadProfileGenerator  --configuration Release --no-self-contained --verbosity quiet -p:DebugType=none
-dotnet publish SimulationEngine --configuration Release --no-self-contained --verbosity quiet -p:DebugType=none --runtime win-x64
-dotnet publish simengine2       --configuration Release --self-contained    --verbosity quiet -p:DebugType=none --runtime win-x64
-dotnet publish simengine2       --configuration Release --self-contained    --verbosity quiet -p:DebugType=none --runtime linux-x64
+dotnet publish LoadProfileGenerator --configuration Release --no-self-contained --verbosity quiet -p:DebugType=none || goto :error
+dotnet publish SimulationEngine     --configuration Release --no-self-contained --verbosity quiet -p:DebugType=none --runtime win-x64 || goto :error
+dotnet publish SimEngine2           --configuration Release --self-contained    --verbosity quiet -p:DebugType=none --runtime win-x64 || goto :error
+dotnet publish SimEngine2           --configuration Release --self-contained    --verbosity quiet -p:DebugType=none --runtime linux-x64 || goto :error
 pause
 
 REM run release checks and collect release files
-dotnet build ReleaseMaker --configuration Debug -t:rebuild  -v:m
+dotnet build ReleaseMaker --configuration Debug -t:rebuild  -v:m || goto :error
 cd /D %srcdirectory%\ReleaseMaker\bin\Debug\%dotnetversion%-windows
-releasemaker
+releasemaker || goto :error
 pause
 
 
@@ -33,7 +34,15 @@ set "releasedirectory=%srcdirectory%\LPGRelease\release_directories"
 set "pylpgdirectory=%srcdirectory%\LPGRelease\pylpg\"
 
 cd /D %releasedirectory%\windows
-simulationengine CreatePythonBindings
-xcopy lpgdata.py %pylpgdirectory%
-xcopy lpgpythonbindings.py %pylpgdirectory%
+simulationengine CreatePythonBindings || goto :error
+move lpgdata.py %pylpgdirectory%
+move lpgpythonbindings.py %pylpgdirectory%
 pause
+exit /b 0
+
+REM error handling
+:error
+echo.
+echo *** RELEASE FAILED (exit code %errorlevel%) ***
+pause
+exit /b 1
