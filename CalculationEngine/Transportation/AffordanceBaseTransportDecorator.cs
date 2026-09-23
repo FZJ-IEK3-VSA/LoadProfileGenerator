22edeff52e5dd9dc764433e4466477980adfc75d
David Neuroth <d.neuroth@fz-juelich.de>
2026-09-23 17:01:54 +0200

    remove obsolete entries from csproj files

--- CalculationEngine/Transportation/AffordanceBaseTransportDecorator.cs
+++ CalculationEngine/Transportation/AffordanceBaseTransportDecorator.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Speech.Recognition.SrgsGrammar;
 using Automation;
 using Automation.ResultFiles;
 using CalculationEngine.Activities;
